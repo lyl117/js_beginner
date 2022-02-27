96c122852f6e3ced00cde0347513be572759db44
yurimlee <ylee981352@gmail.com>
2022-02-27 23:29:05 +0900

    [#19] 그림판 만들기 - 2 (Canvas Events)

---
+++ PaintJs/app.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function stopPainting(){
+  painting = false;
+}
+
+function onMouseMove(event){
+  const x = event.offsetX;
+  const y = event.offsetY;
+}
+function onMouseDown(evnet){
+  console.log(true);
+}
+function onMouseUp(evnet){
+  stopPainting();
+}
+
+if(canvas){
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
