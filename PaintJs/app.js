f3a737a6a591a47323c0a3a95dfcb59ee51b9d4f
yurimlee <ylee981352@gmail.com>
2022-02-28 22:16:57 +0900

    [#19] painting(Canvas Api)

--- PaintJs/app.js
+++ PaintJs/app.js
@@ -1,25 +1,41 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width=700;
+canvas.height = 700;
+
+ctx.strokeStyle = "##2c2c2c"; // 선의 색깔
+ctx.lineWidth = 2.5; //선의 넓이
 
 let painting = false;
 
 function stopPainting(){
-  painting = false;
+  painting = true;
+}
+
+function startPainting(){
+  painting = true;
 }
 
 function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;
+  if(!painting){
+    ctx.beginPath(); //경로 생성
+    ctx.moveTo(x,y); //선 시작 좌표
+  } else {
+    ctx.lineTo(x,y); //선 끝 좌표
+    ctx.stroke(); //선 그리기 
+  } //마우스가 움직이는 동안 선이 만들어진다.
 }
-function onMouseDown(evnet){
-  console.log(true);
+function onMouseDown(event){
+  painting = true;
 }
-function onMouseUp(evnet){
-  stopPainting();
-}
+
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
