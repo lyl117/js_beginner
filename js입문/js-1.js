c3688bef529e448356f08c355b476a7614315cb8
yurimlee <ylee981352@gmail.com>
2022-03-08 23:24:32 +0900

    [#18] js-1(Object,Math,parseInt,parseFloat etc)

--- js입문/js-1.js
+++ js입문/js-1.js
@@ -40,3 +40,57 @@ document.write(fruit);
 document.write(fruit[0]);
 fruit[0] = "Tomato";
 document.write(fruit);
+
+//Object 
+var student = {
+  name : "Elice",
+  age: 20,
+  skills: ["Java", "HTML", "CSS","JavaScript"],
+  sum: function(num1,num2) {
+  return num1 + num2;
+  }
+}
+student["name"] = "park";
+document.write(student['name']);
+document.write(student.sum(10,20));
+
+//Undefined, Null
+var str1;
+document.write(str1);
+var empty= null;
+document.write(empty);
+
+//Boolean
+var t = true;
+var f = false;
+document.write(t,f);
+
+//property, method
+//String(length,charAt,split)
+var str1 = "Hello World";
+document.write(str1.length);
+document.write(str1.charAt(0));
+document.write(str1.split(" "));
+
+// arrangement(push,unshift,pop,shift)
+var fruit = ["Apple", "Banana","Tomato"];
+document.write(fruit.length);
+fruit.push("A");
+fruit.unshift("B");
+​
+document.write(fruit);
+fruit.pop("-1");
+fruit.shift("0");
+
+document.write(fruit);
+
+//Math(abs,ceil,floor,random)
+document.write(Math.abs(-3));
+document.write(Math.ceil(0.3));
+document.write(Math.floor(10.9));
+document.write(Math.random());
+
+//parseInt, parseFloat
+var str1 ="20.14";
+document.write(parseInt(str1));
+document.write(parseFloat(str1));
