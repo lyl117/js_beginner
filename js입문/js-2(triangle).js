fa50d2a4ca59f81e2764152420bf2ba4da400c0f
yurimlee <ylee981352@gmail.com>
2022-03-09 00:03:59 +0900

    [#18] Finding the area of the triangle, Accessing array data in an array

---
+++ js입문/js-2(triangle).js
@@ -0,0 +1,14 @@
+//Finding the area of the triangle.
+function triangle(width, height) {
+  return width * height /2 ;
+ }
+document.write(triangle(5, 7));
+
+// Accessing array data in an array
+var arrTest = [
+  [100, 200, 300],
+  [1000, 2000, 3000],
+  [1111, 2222, 3333]
+];
+
+document.write(arrTest[1][2]);  // 3000 출력
