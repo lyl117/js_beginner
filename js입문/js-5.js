308507bdf1d246d5280398fd9d47cb684132813c
yurimlee <ylee981352@gmail.com>
2022-03-11 20:39:21 +0900

    [#18] js-5(JavaScript question collection)

---
+++ js입문/js-5.js
@@ -0,0 +1,29 @@
+//Paying for food.
+//Korean food prices.
+var korean = 7000;
+
+// Chiness food prices.
+var chinese = 6000;
+
+// Western food prices.
+var western = 8500;
+
+// The discounted price of Korean food.
+var discount_korean = 6300;
+
+// Calculate and save the entire budget.
+var total_price = 1046500;
+
+// check the price
+console.log("한식 : " + korean)
+console.log("중식 : " + chinese)
+console.log("양식 : " + western)
+console.log("할인된 한식 : " + discount_korean)
+console.log("전체 예산 : " + total_price)
+
+//Find the share and the rest
+var num1 = 23781367;
+var num2 = 1754;
+
+console.log(Math.floor(num1/ num2));
+console.log(Math.floor(num1%num2));
