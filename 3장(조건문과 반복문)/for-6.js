45dcf9f4f5dd73fc931adbc6a1b99c9cbece46c3
[frontend09] <ylee981352@gmail.com>
2022-05-03 23:56:29 +0900

    [#7] for문 연습

---
+++ 3장(조건문과 반복문)/for-6.js
@@ -0,0 +1,6 @@
+let fruits = ["apple", "orange", "cherry"];
+
+// for
+for(let i =0; i < fruits.length; i++){
+  // fruits[0] let i = 2; i = i +2; i+=2
+  document.getElementById('for').innerHTML += '<li>' + fruits[i]+'</li>'}
