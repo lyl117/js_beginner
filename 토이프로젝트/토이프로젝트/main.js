4ee5d06dc0a5f0a9a9f9662f9609d685dad204df
[frontend09] <ylee981352@gmail.com>
2022-05-08 23:04:31 +0900

    [#24] 새로운 파일에 다시 만들기

--- 토이프로젝트/토이프로젝트/main.js
+++ 토이프로젝트/토이프로젝트/main.js
@@ -1,30 +1,29 @@
-
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
   sliderPerView: 1,
   spaceBetween: 10,
   centeredSlides: true,
   grabCursor: true,
-  
+
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
-  observer: true,	// 추가
+  observer: true, // 추가
   observeParents: true,
   variableWidth: true,
 });
